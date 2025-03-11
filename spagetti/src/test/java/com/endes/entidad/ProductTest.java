d7aada98f9f34689ff578a760fbf5c101fa47000
pedronoguera04 <pedro.noguera.sanchez@iescamas.es>
2025-03-11 12:32:17 +0100

    creada rama desarrollo para hacer pruebas y practicar

---
+++ spagetti/src/test/java/com/endes/entidad/ProductTest.java
@@ -0,0 +1,19 @@
+package com.endes.entidad;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ProductTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
